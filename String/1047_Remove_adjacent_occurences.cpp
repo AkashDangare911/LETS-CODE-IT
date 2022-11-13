0ee4fd98e821c3919edbabbffdc1e3460265ec1a
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2022-11-13 22:38:52 +0530

    Update 1047_Remove_adjacent_occurences.cpp

--- String/1047_Remove_adjacent_occurences.cpp
+++ String/1047_Remove_adjacent_occurences.cpp
@@ -1,7 +1,6 @@
-/*You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
-
+/* PROBLEM STATEMENT -
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 We repeatedly make duplicate removals on s until we no longer can.
-
 Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.*/
 
 #include <bits/stdc++.h>
@@ -9,6 +8,7 @@ using namespace std;
 
 string solve(string s)
 {
+    // use stack to store all characters
     stack<char> st;
 
     for (int i = s.size() - 1; i >= 0; i--)
@@ -17,7 +17,7 @@ string solve(string s)
         {
             st.push(s[i]);
         }
-        else if (st.top() == s[i])
+        else if (st.top() == s[i])      // if any similar characters appear, pop them
         {
             st.pop();
         }
@@ -26,7 +26,8 @@ string solve(string s)
             st.push(s[i]);
         }
     }
-
+    
+    // finally form a string from remaining characters from stack , which will store the ans 
     string ans = "";
     while (st.size() != 0)
     {
